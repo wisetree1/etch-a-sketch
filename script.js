8cd8446438bf1672d4c23ac16ffda4e642398cda
illy <f0luxeq@gmail.com>
2025-11-18 18:42:42 +0300

    Add basic HTML UI, support for color picking and showing/hiding grid

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 'use strict';
 
-const cells = [];
-const drawingBoard = document.body.querySelector('.drawing-board');
+function generateDrawingBoard() {
+    cells = [];
 
-let gridSize = 16;
+    const drawingBoard = document.createElement('div');
+    drawingBoard.classList.add('drawing-board');
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.className = 'grid-row';
+    const cellWidth = drawingBoardSize / gridSize;
 
-    for (let j = 0; j < gridSize; j++) {
-        const cell = document.createElement('div');
-        cell.className = `grid-cell cell${gridSize * i + j}`;
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.className = 'grid-row';
 
-        row.appendChild(cell);
-        cells.push(cell);
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.className = `grid-cell cell${gridSize * i + j}`;
+            cell.setAttribute('style', `width: ${cellWidth}px; height: ${cellWidth}px;`);
+
+            row.appendChild(cell);
+            cells.push(cell);
+        }
+        drawingBoard.appendChild(row);
     }
-    drawingBoard.appendChild(row);
+
+    drawingBoard.onmousemove = (e) => { paintCell(e) };
+    drawingBoard.onmousedown = (e) => { paintCell(e) };
+
+    gameContainer.appendChild(drawingBoard);
+    return drawingBoard;
 }
 
-let lastCellIndex = '';
-drawingBoard.addEventListener('mousemove', (e) => {
-    const cellIndex = e.target.classList.item(1).split('cell')[1];
-    if (cellIndex == lastCellIndex) {
+function paintCell(e) {
+    // check whether the primary button was pressed
+    if (+e.buttons != 1) {
         return;
-    } else if (lastCellIndex != '') {
-        cells[+lastCellIndex].classList.toggle('active');
     }
-    lastCellIndex = cellIndex;
 
-    cells[+cellIndex].classList.toggle('active');
-});
+    const cellIndex = e.target.classList.item(1).split('cell')[1];
+    cells[+cellIndex].style.backgroundColor = color;
+}
+
+function toggleGrid() {
+    cells.forEach((item) => {
+        item.style.border = isGridOn ? 'none' : '0.5px solid rgb(0, 0, 0)';
+    });
+    isGridOn = !isGridOn;
+}
+
+const drawingBoardSize = 500;
+let gridSize = 16;
+let cells = [];
+
+const gameContainer = document.body.querySelector('.game-container');
+const drawingBoard = generateDrawingBoard();
+const optionsPanel = document.body.querySelector('.options-panel');
+
+let color = '#000000';
+const colorPicker = optionsPanel.querySelector('.color-picker');
+colorPicker.oninput = (e) => { color = e.target.value };
+
+let isGridOn = false;
+const gridBtn = optionsPanel.querySelector('.grid-btn');
+gridBtn.onclick = () => toggleGrid();
